7f8a541a699dc02682e8e90afb6c64bd7a594bd8
dfinel <ledan.finel@gmail.com>
2023-11-29 10:58:11 +0100

    add gitignore and modified text_ops to print content of all files

--- text_ops.sh
+++ text_ops.sh
@@ -2,3 +2,6 @@
 grep -c "function" README.md >> output/function-keyword-count.txt
 grep -o "^#.*" README.md | sed "s/^#*//" | sort -u > output/unique-headers.txt
 grep -o "\d" README.md | sort -n > output/sorted-numbers.txt
+grep "Calculator" README.md | sort -r > output/calculator-lines.txt
+cat output/*.txt
+
