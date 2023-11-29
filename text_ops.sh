a73de2b7f338d8e64fc60ed87a11b0bfca754949
dfinel <ledan.finel@gmail.com>
2023-11-29 10:20:50 +0100

    add sorted_numbers features-summary and calculator-lines

--- text_ops.sh
+++ text_ops.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 grep -c "function" README.md >> output/function-keyword-count.txt
 grep -o "^#.*" README.md | sed "s/^#*//" | sort -u > output/unique-headers.txt
+grep -o "\d" README.md | sort -n > output/sorted-numbers.txt
